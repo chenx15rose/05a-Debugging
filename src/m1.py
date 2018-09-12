5d21da58f50f5221484294a7b417fa04a92a8f23
chenx15 <chenx15@rose-hulman.edu>
2018-09-11 20:50:07 -0400

    finished

--- src/m1.py
+++ src/m1.py
@@ -311,7 +311,7 @@ def broken_6(n):
 
 
 # ----------------------------------------------------------------------
-# TODO: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # ----------------------------------------------------------------------
 def broken_7(n, point, length, distance_between_lines, window):
@@ -346,7 +346,6 @@ def broken_7(n, point, length, distance_between_lines, window):
         window.render(0.5)
         left = rg.Point(left.x, left.y + distance_between_lines)
         right = rg.Point(right.x, right.y + distance_between_lines)
-
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
